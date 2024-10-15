87a12737810552eba5a8c8f622a283fc9317100e
masonempey <149198668+masonempey@users.noreply.github.com>
2024-10-15 15:36:34 -0600

    Few changes

---
+++ src/Main.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+function Main(): React.JSX.Element {
+  return (
+    <View>
+      <Text>Main Component</Text>
+    </View>
+  );
+}
+
+export default Main;
